39213014971e68779eda3502fa5cac3d39e1cf5e
Finngodumb <finny12349@gmail.com>
2023-05-21 12:10:17 -0400

    debugging

--- server.js
+++ server.js
@@ -22,3 +22,4 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 app.listen(PORT, console.log(`listening on port ${PORT}`))// npx nodemon index.js
 
+module.exports = app
